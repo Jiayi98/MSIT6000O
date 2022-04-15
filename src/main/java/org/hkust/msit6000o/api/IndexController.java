3ca831e297898f79dcf2cdd68218a809453aa951
Xenon <s10110040@gmail.com>
2022-04-15 23:14:37 +0800

    debug

--- src/main/java/org/hkust/msit6000o/api/IndexController.java
+++ src/main/java/org/hkust/msit6000o/api/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("/index")
-@RestController
+@RequestMapping("/")
+@Controller
 public class IndexController {
 
     @GetMapping
--- src/main/java/org/hkust/msit6000o/api/ModalController.java
+++ src/main/java/org/hkust/msit6000o/api/ModalController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/model/")
-@RestController
+@Controller
+@RequestMapping("modals")
 public class ModalController {
+
     @GetMapping("modal1")
     public String modal1() {
         return "modal1";
