301519aab0491d5bc4aa4ddcd2f3a3e11342ca41
jiayi98 <judyzhou959@utexas.edu>
2022-04-15 20:50:32 +0800

    description

---
+++ src/test/java/org/hkust/msit6000o/Msit6000oApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hkust.msit6000o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msit6000oApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
