e70ed83f96ac5832ab83e81d0eb2d12bc43be1ed
jiayi98 <judyzhou959@utexas.edu>
2022-04-16 17:55:47 +0800

    Dev: finish upload to S3 and S3 config

--- src/main/java/org/hkust/msit6000o/service/FileService.java
+++ src/main/java/org/hkust/msit6000o/service/FileService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 // file upload and S3 connection service
 @Service
@@ -25,27 +27,42 @@ public class FileService {
 
     //upload file to backend
     public void uploadFile(MultipartFile file, String fileName) throws Exception {
-        File targetFile = new File(filePath);
-        if (!targetFile.exists()) {
-            targetFile.mkdirs();
-        }
-        FileOutputStream out = new FileOutputStream(filePath + fileName);
-        out.write(file.getBytes());
-        out.flush();
-        out.close();
+        // 1. 调用spark来process 文件
+        // 2. 将上一步的结果上传到s3
+        String temp_filename = file.getOriginalFilename().replace(".csv", "_result.txt");
+        System.out.println("上传成功啦，进入s3处理函数");
+//        process(convertMultiPartFileToFile(file), temp_filename);
+        uploadFileToS3(temp_filename);
+    }
 
-        // Pending FileUploadImp implementation
-        // uploadFileToS3(file);
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            System.out.println("Error converting multipartFile to file");
+        }
+        return convertedFile;
+    }
+
+    public void process(File file, String temp_filename) throws FileNotFoundException {
+        //数据分析
+        //将结果写入txt文件
+        // spark.........write(filePath)
+
+
     }
 
     //upload file to S3
     public boolean uploadFileToS3(String filename){
-        return fileDao.fileUpload(filePath + filename);
+        return fileDao.fileUpload(filePath + "test.txt");
     }
 
     //Get url from S3 by filename
     public String getFileUrl(String filename){
         return fileDao.getFileUrl(filename);
     }
+
+
 
 }
